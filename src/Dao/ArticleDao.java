a6f2118b5f87dfd239c4f1c803cfe546f740da6e
wlm <86889>
2019-09-22 17:11:51 +0800

    dbutil

--- src/Dao/ArticleDao.java
+++ src/Dao/ArticleDao.java
@@ -2,6 +2,7 @@ package Dao;
 
 import Util.DbUtil;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
 
 import java.sql.SQLException;
 
@@ -9,6 +10,8 @@ public class ArticleDao {
     public void AddArticle(){
         QueryRunner queryRunner=new QueryRunner();
         try {
+            ResultSetHandler
+            queryRunner.;
             queryRunner.update(DbUtil.getCurrentConn(),"");
         } catch (SQLException e) {
             e.printStackTrace();
