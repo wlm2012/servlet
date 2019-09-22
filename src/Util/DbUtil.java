0d583afacb4f1f9848a7d649f51e95bbea17c057
wlm <86889>
2019-09-22 11:18:15 +0800

    dbutil

--- src/Util/DbUtil.java
+++ src/Util/DbUtil.java
@@ -1,4 +1,49 @@
 package Util;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class DbUtil {
+
+    private static String drivename;
+    private static String url;
+    private static String userName;
+    private static String passWord;
+    private static Connection conn = null;
+    private static ThreadLocal<Connection> tl = new ThreadLocal<>();
+
+    static {
+        Properties properties = new Properties();
+        try {
+            InputStream in = new BufferedInputStream(new FileInputStream("resources/mysql.properties"));
+            properties.load(in);
+
+            drivename = properties.getProperty("drivename");
+            url = properties.getProperty("url");
+            userName = properties.getProperty("userName");
+            passWord = properties.getProperty("passWord");
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConn() throws SQLException {
+        if (null == conn || conn.isClosed()) {
+            try {
+                Class.forName(drivename);
+                conn = DriverManager.getConnection(url, userName, passWord);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return conn;
+    }
+
+
 }
