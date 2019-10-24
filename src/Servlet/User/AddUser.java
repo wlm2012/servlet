2b0d7333e7f5d1d8ccba85e58e9aeaf821cdb8e7
wlm <wangliangm2012@gmail.com>
2019-10-24 10:32:32 +0800

    用户注册

---
+++ src/Servlet/User/AddUser.java
@@ -0,0 +1,20 @@
+package Servlet.User;
+
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/AddUser")
+public class AddUser extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp){
+        doPost(req,resp);
+    }
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp){
+
+    }
+}
